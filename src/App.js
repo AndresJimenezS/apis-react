f9e508a44dfdaf54c5e42f1a77c91f020a37d223
AndresJimenezS <andresjsolis@gmail.com>
2024-05-14 15:35:15 -0600

    Navigation / Routing with React { LINKS }

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import React, {Fragment} from 'react';
 
+// Routing
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 /* Layout */
 import Header from './componentes/layout/Header'
+import Navegacion from './componentes/layout/Navegacion'
+
+/* Componentes */
+import Clientes from './componentes/clientes/Clientes'
+import Pedidos from './componentes/pedidos/Pedidos'
+import Productos from './componentes/productos/Productos'
 
 
 function App(){
   return(
-    // sintaxis utilizar componentes(fragment)
-    <Fragment>
+    <BrowserRouter>  {/* envolver todo el código para utilizar routing*/}
+
+    <Fragment> {/* sintaxis para utilizar varios componentes con fragment*/}
       <Header />
+
+        <div className="grid contenedor contenido-principal">
+          <Navegacion />
+
+          <main class="caja-contenido col-9">
+
+            <Routes>
+              <Route path="/" element={<Clientes />} />
+
+              <Route path="/productos" element={<Productos />} />
+
+              <Route path="/pedidos" element={<Pedidos />} />
+
+            </Routes>
+          </main>
+        </div>
+
     </Fragment>
+    </BrowserRouter>
+
   )
 }
 
